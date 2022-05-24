95c6a5067e316f677ec72b645746a816fc85cd87
zoya <zoya.v@ya.ru>
2022-05-24 16:19:06 +0800

    make agent initialization

--- utils/step_by_step_enjoy.py
+++ utils/step_by_step_enjoy.py
@@ -127,6 +127,17 @@ def download_weights():
              'TreeChopBaseline-iglu/checkpoint_p0/')
     print("Weights path - ", artifact_dir)
 
+def make_agent():
+    register_custom_components()
+    env = make_iglu()
+    cfg = parse_args(argv=['--algo=APPO', '--env=IGLUSilentBuilder-v0', '--experiment=TreeChopBaseline-iglu',
+                           '--experiments_root=force_envs_single_thread=False;num_envs_per_worker=1;num_workers=10',
+                           '--train_dir=../train_dir/0012'], evaluation=True)
+    cfg = load_from_checkpoint(cfg)
+
+    cfg.setdefault("path_to_weights",
+                   "../train_dir/0012/force_envs_single_thread=False;num_envs_per_worker=1;num_workers=10/TreeChopBaseline-iglu")
+    return APPOHolder(cfg)
 
 if __name__ == "__main__":
     download_weights()
