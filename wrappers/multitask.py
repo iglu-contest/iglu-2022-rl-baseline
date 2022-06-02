fix hole calculation

wrappers/multitask.py
@@ -51,7 +51,7 @@ class SubtaskGenerator(gym.Wrapper):
         self.old_grid = np.zeros((9, 11, 11))
         self.preinited_grid = None
         self.old_preinited_grid = None
-        self.prebuilds_percent = 0.7
+        self.prebuilds_percent = 0.5
         self.last_agent_rotation = (0, 0)
         self.last_target = None
         self.color_map = None

wrappers/target_generator.py
@@ -124,10 +124,10 @@ class RandomFigure(Figure):
         figure[fig_filter] = 1
 
         relief = figure.sum(axis = 0)
-        high_blocks = np.where(relief > 3)
+        high_blocks = np.where(relief > 1)
         holes_indx = [[],[],[]]
         for x, y in zip(*high_blocks):
-            z = np.random.randint(1,relief[x,y]-1)
+            z = np.random.randint(0,relief[x,y]-1)
             holes_indx[0].append(z)
             holes_indx[1].append(x)
             holes_indx[2].append(y)