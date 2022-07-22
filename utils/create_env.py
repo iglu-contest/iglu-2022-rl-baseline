d2fdf7bb69a6e3083398d99f9e83f3cd0014b554
Zoya Volovikova <zvolovikova@cds2.isa.ru>
2022-07-22 11:33:19 +0300

    fix error with obs in sf

--- utils/create_env.py
+++ utils/create_env.py
@@ -47,7 +47,7 @@ def make_iglu(*args, **kwargs):
     env = JumpAfterPlace(env)
     env = RangetRewardFilledField(env)
     env = Closeness(env)
-
+    env = FakeObsWrapper(env)
     env = SuccessRateFullFigure(env)
     env = MultiAgentWrapper(env)
     env = AutoResetWrapper(env)
